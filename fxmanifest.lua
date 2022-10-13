4b925fd52bc8fe97376eca2101a5d197c2b57e46
RexShack <rexshackuk@gmail.com>
2022-10-13 17:12:29 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version "adamant"
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 game "rdr3"
 
-description 'MAP-Moonshineshacks'
+description 'map-moonshineshacks'
 
 client_scripts {
     'client/client.lua',
